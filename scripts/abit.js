6586f1332c19148c496fa719a42ace024f55f244
nator99-b <66281656+nator99-b@users.noreply.github.com>
2020-06-05 03:46:52 +0100

    My update

--- scripts/abit.js
+++ scripts/abit.js
@@ -1,7 +1,7 @@
-const name = "Sikiru Abidemi Tiamiyu";
+const name = "Nator oruama";
 const id = "HNG-00682";
-const lang = "JavaScript";
-const email = "tiamiyusikiruabidemi@gmail.com";
+const lang = "mobile dev";
+const email = "oruama4me@yahoo.com";
 
 let sentence = `Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task.${email}`;
 
